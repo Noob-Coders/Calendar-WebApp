1f26b158996c6dd80d76fd244bd2643dc0822a5a
Ankit Kumar <akwelcomes1221@gmail.com>
2020-03-10 21:55:59 +0530

    tracked deleted files

--- scripts/calendar-dictionary.js
+++
@@ -1,4 +0,0 @@
-var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-export { days, months };
